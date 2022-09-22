d48c5a133746e99ba417cd71fe4470a18ecf4a6c
BpoTmHeHoru <98018091+BpoTmHeHoru@users.noreply.github.com>
2022-09-22 14:29:24 +0400

    Add files via upload

--- NetworkTest/start (2).bat
+++ NetworkTest/start (2).bat
@@ -7,4 +7,4 @@ start cmd.exe /C python.exe start.py
 cd ../node2
 start cmd.exe /C python.exe start.py
 
-start cmd.exe /C geth attach http://192.168.10.191:8545 --exec miner.start(1)
+start cmd.exe /C geth attach http://192.168.0.102:8545 --exec miner.start(1)
